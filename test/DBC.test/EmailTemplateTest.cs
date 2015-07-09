24d1e68a2382c41a3e41c005af13330d39e82535
ThomKiesewetterPrive <thom@kiesewetter.nl>
2015-07-09 17:08:07 +0200

    restructure project

---
+++ test/DBC.test/EmailTemplateTest.cs
@@ -0,0 +1,101 @@
+﻿//using System;
+//using System.IO;
+//using System.Reflection;
+//using System.Runtime.Versioning;
+//using DBC.Services;
+//using Microsoft.AspNet.Hosting;
+//using Microsoft.AspNet.Http;
+//using Microsoft.AspNet.Http.Core;
+//using Microsoft.AspNet.Mvc;
+//using Microsoft.AspNet.Mvc.OptionDescriptors;
+//using Microsoft.AspNet.Mvc.Razor;
+//using Microsoft.AspNet.Mvc.Rendering;
+//using Microsoft.Framework.DependencyInjection;
+//using Microsoft.Framework.OptionsModel;
+//using Microsoft.Framework.Runtime;
+//using Moq;
+//using Xunit;
+
+//namespace DBC.test
+//{
+//    public class EmailTemplateTest
+//    {
+//        public void test()
+//        {
+//            IServiceCollection services = new ServiceCollection();
+
+//            var currentHostingEnvironment = TestHelper.GetCurrentHostingEnvironment(nameof(DBC));
+//            new Startup(currentHostingEnvironment).ConfigureServices(services);
+//            var assemblyProvider = TestHelper.CreateAssemblyProvider(nameof(DBC));
+//            services.AddInstance(assemblyProvider);
+//            services.AddInstance(GetLoadContextAccessor());
+//            services.AddInstance(TestHelper.GetCurrentApplicationEnvironment(nameof(DBC)));
+
+//            IServiceProvider serviceProvider = services.BuildServiceProvider();
+//            ITypeActivatorCache typeActivatorCache = services.BuildServiceProvider().GetRequiredService<ITypeActivatorCache>();//new DefaultTypeActivatorCache();
+//            IOptions<MvcOptions> optionsAccessor = services.BuildServiceProvider().GetRequiredService<IOptions<MvcOptions>>();
+//            IHttpContextAccessor c  = services.BuildServiceProvider().GetRequiredService< IHttpContextAccessor>();
+//            IViewEngineProvider viewEngineProvider = services.BuildServiceProvider().GetRequiredService<IViewEngineProvider>();
+//            ICompositeViewEngine a = services.BuildServiceProvider().GetRequiredService<ICompositeViewEngine>();
+//            //var optionsSetup = new MvcOptionsSetup();
+//            //var options = new MvcOptions();
+//            //optionsSetup.Configure(options);
+//            //IOptions<MvcOptions> optionsAccessor = new MyOptions<MvcOptions>(options);
+//            //IHttpContextAccessor c = new MyHttpContextAccessor();
+//            //IViewEngineProvider viewEngineProvider = new DefaultViewEngineProvider(optionsAccessor, typeActivatorCache, serviceProvider);
+//            //ICompositeViewEngine a = new CompositeViewEngine(viewEngineProvider);
+//            var b = new EmailTemplate(a, c);
+//        }
+
+//        public class MyHttpContextAccessor : IHttpContextAccessor
+//        {
+//            public MyHttpContextAccessor()
+//            {
+//                HttpContext = new DefaultHttpContext();
+//            }
+//            public HttpContext HttpContext { get; set; }
+//        }
+//        public class MyOptions<TOptions> : IOptions<TOptions> where TOptions : class, new()
+//        {
+//            public MyOptions(TOptions options)
+//            {
+//                Options = options;
+//            }
+//            public TOptions GetNamedOptions(string name)
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public TOptions Options { get; }
+//        }
+//        private static IAssemblyLoadContextAccessor GetLoadContextAccessor()
+//        {
+//            var loadContext = new Mock<IAssemblyLoadContext>();
+//            loadContext.Setup(s => s.LoadStream(It.IsAny<Stream>(), It.IsAny<Stream>()))
+//                       .Returns((Stream stream, Stream pdb) =>
+//                       {
+//                           var memoryStream = (MemoryStream)stream;
+//                           return Assembly.Load(memoryStream.ToArray());
+//                       });
+
+//            var accessor = new Mock<IAssemblyLoadContextAccessor>();
+//            accessor.Setup(a => a.GetLoadContext(typeof(RoslynCompilationService).Assembly))
+//                    .Returns(loadContext.Object);
+//            return accessor.Object;
+//        }
+//        private IApplicationEnvironment GetApplicationEnvironment()
+//        {
+//            var applicationEnvironment = new Mock<IApplicationEnvironment>();
+//            applicationEnvironment.SetupGet(a => a.ApplicationName)
+//                                  .Returns("MyApp");
+//            applicationEnvironment.SetupGet(a => a.RuntimeFramework)
+//                                  .Returns(new FrameworkName("ASPNET", new Version(4,5,1)));
+//            applicationEnvironment.SetupGet(a => a.Configuration)
+//                                  .Returns("Debug");
+//            applicationEnvironment.SetupGet(a => a.ApplicationBasePath)
+//                                  .Returns("MyBasePath");
+
+//            return applicationEnvironment.Object;
+//        }
+//    }
+//}
---
+++ test/DBC.test/TestHelper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Infrastructure;
+
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace DBC.test
+{
+    public static class TestHelper
+    {
+        // Path from Mvc\\test\\Microsoft.AspNet.Mvc.FunctionalTests
+
+        public static TestServer SetupServer(string applicationWebSiteName, Func<Func<Task>, HttpContext, Task> testMiddlewareFunc)
+        {
+            var currentHostingEnvironment = GetCurrentHostingEnvironment(applicationWebSiteName);
+            Action<IApplicationBuilder> appBuilder = new Startup(currentHostingEnvironment).SetupRequestPipeline;
+            Action<IServiceCollection> configureServices = new Startup(currentHostingEnvironment).ConfigureServices;
+            return TestHelper.CreateServer(app =>
+                AddTestUses(app, appBuilder, testMiddlewareFunc),
+                applicationWebSiteName, "../src", configureServices);
+        }
+
+        public static HostingEnvironment GetCurrentHostingEnvironment(string applicationWebSiteName)
+        {
+            //var hostingEnvironment = new HostingEnvironment(GetCurrentApplicationEnvironment(applicationWebSiteName));
+            var env = GetCurrentApplicationEnvironment(applicationWebSiteName);
+            var hostingEnvironment = new HostingEnvironment();
+            hostingEnvironment.Initialize(env.ApplicationBasePath, environmentName: null);
+            return hostingEnvironment;
+        }
+
+        public static TestApplicationEnvironment GetCurrentApplicationEnvironment(string applicationWebSiteName)
+        {
+            var curPath = Environment.CurrentDirectory;
+            var path = Path.GetFullPath(Path.Combine(curPath, "..", "src", applicationWebSiteName));
+            return new TestApplicationEnvironment(path, "DBC", "1.0.0", "DEBUG",
+                new FrameworkName("DNX,Version=v4.5.1"));
+        }
+
+        public static TestServer CreateServer(Action<IApplicationBuilder> builder, string applicationWebSiteName,
+            string applicationPath)
+        {
+            return CreateServer(builder, applicationWebSiteName, applicationPath, (Action<IServiceCollection>) null);
+        }
+
+        public static TestServer CreateServer(Action<IApplicationBuilder> builder, string applicationWebSiteName,
+            string applicationPath, Action<IServiceCollection> configureServices)
+        {
+            return TestServer.Create(builder,
+                services => AddTestServices(services, applicationWebSiteName, applicationPath, configureServices));
+        }
+
+        public static TestServer CreateServer(Action<IApplicationBuilder> builder, string applicationWebSiteName,
+            string applicationPath, Func<IServiceCollection, IServiceProvider> configureServices)
+        {
+            return TestServer.Create(CallContextServiceLocator.Locator.ServiceProvider, builder,
+                services =>
+                {
+                    AddTestServices(services, applicationWebSiteName, applicationPath, null);
+                    return (configureServices != null) ? configureServices(services) : services.BuildServiceProvider();
+                });
+        }
+
+        private static void AddTestUses(IApplicationBuilder app, Action<IApplicationBuilder> builder, Func<Func<Task>, HttpContext, Task> getValue)
+        {
+            app.Use(async (context, next) =>
+            {
+                await getValue(next, context);
+            });
+            builder?.Invoke(app);
+        }
+        private static void AddTestServices(IServiceCollection services, string applicationWebSiteName,
+            string applicationPath, Action<IServiceCollection> configureServices)
+        {
+            // Get current IApplicationEnvironment; likely added by the host.
+            var provider = services.BuildServiceProvider();
+            var originalEnvironment = provider.GetRequiredService<IApplicationEnvironment>();
+
+            // When an application executes in a regular context, the application base path points to the root
+            // directory where the application is located, for example MvcSample.Web. However, when executing
+            // an application as part of a test, the ApplicationBasePath of the IApplicationEnvironment points
+            // to the root folder of the test project.
+            // To compensate for this, we need to calculate the original path and override the application
+            // environment value so that components like the view engine work properly in the context of the
+            // test.
+            var applicationBasePath = CalculateApplicationBasePath(originalEnvironment, applicationWebSiteName,
+                applicationPath);
+            var environment = new TestApplicationEnvironment(originalEnvironment, applicationBasePath,
+                applicationWebSiteName);
+            services.AddInstance<IApplicationEnvironment>(environment);
+            var hostingEnvironment = new HostingEnvironment();
+            hostingEnvironment.Initialize(applicationBasePath, environmentName: null);
+            services.AddInstance<IHostingEnvironment>(hostingEnvironment);
+            //beta 6?
+            //services.AddInstance<IHostingEnvironment>(new HostingEnvironment(environment)); 
+
+            // Injecting a custom assembly provider. Overrides AddMvc() because that uses TryAdd().
+            var assemblyProvider = CreateAssemblyProvider(applicationWebSiteName);
+            services.AddInstance(assemblyProvider);
+
+            configureServices?.Invoke(services);
+        }
+
+        // Calculate the path relative to the application base path.
+        private static string CalculateApplicationBasePath(
+            IApplicationEnvironment appEnvironment,
+            string applicationWebSiteName,
+            string websitePath)
+        {
+            // Mvc/test/WebSites/applicationWebSiteName
+            return Path.GetFullPath(
+                Path.Combine(appEnvironment.ApplicationBasePath, websitePath, applicationWebSiteName));
+        }
+
+        public static IAssemblyProvider CreateAssemblyProvider(string siteName)
+        {
+            // Creates a service type that will limit MVC to only the controllers in the test site.
+            // We only want this to happen when running in-process.
+            var assembly = Assembly.Load(new AssemblyName(siteName));
+            var provider = new FixedSetAssemblyProvider
+            {
+                CandidateAssemblies =
+                {
+                    assembly
+                }
+            };
+
+            return provider;
+        }
+    }
+}
